06c84484a2d5a9397134abedfcaed0a7bc8bbac5
strance02 <94926003+strance02@users.noreply.github.com>
2021-12-04 08:25:31 +0100

    Add files via upload

--- puzzle6_code.py
+++ puzzle6_code.py
@@ -1,18 +1,19 @@
 with open('day3_input.txt') as f:
 		data=data_fix=f.readlines()
 
+#CALULATE OXYGEN
 length=len(data[0])
-for x in range(length-1):
+for x in range(length-1): #why is it necessary to subtract 1??
 	zero_count = one_count = 0
 	data_zero=[]
 	data_one=[]
 	for number in data:
 		if number[x]=='0':
 			zero_count+=1
-			data_zero.append(number)
+			data_zero.append(number) #making a list with the zero bit numbers
 		else:
 			one_count+=1
-			data_one.append(number)
+			data_one.append(number) #making a list with the one bit numbers
 	if zero_count > one_count:
 		data=data_zero
 	else:
@@ -24,6 +25,7 @@ oxygen=data[0]
 
 print(int(oxygen,2))
 
+#CALCULATE CO2
 data=data_fix
 for x in range(length-1):
 	zero_count = one_count = 0
